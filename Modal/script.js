cdd4ef5fee217746ee0803f557abd915e7f2b958
cmd-sachin <sachiniyyappan2005@gmail.com>
2024-10-04 22:38:53 +0530

    Final

---
+++ Modal/script.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const close_modal = document.querySelector(".close-modal");
+const show_modal = document.querySelectorAll(".show-modal");
+
+const closeModal = () => {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+const openModal = () => {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+  //modal.style.display = "block";
+  //overlay.style.display = "block";
+};
+
+for (let i = 0; i < show_modal.length; i++) {
+  show_modal[i].addEventListener("click", openModal);
+}
+overlay.addEventListener("click", closeModal);
+close_modal.addEventListener("click", closeModal);
+document.addEventListener("keydown", (e) => {
+  if (e.key == "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
